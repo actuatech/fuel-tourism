f96a967dc187db81ecef3383fa0ddedf1e0aeaa5
escardill <nil.celisfont@actuatech.ad>
2021-05-03 11:50:34 +0200

    Filter by day greater than added

--- DataCleaning/Filters.py
+++ DataCleaning/Filters.py
@@ -1,5 +1,6 @@
 from datetime import datetime
+import pandas as pd
 
 
-def filter_by_year(registres: pd.DataFrame, colname: str) -> pd.DataFrame:
-    return registres[registres[colname] > datetime(1990, 1, 1)]
+def filter_by_year_greater_than(registres: pd.DataFrame, colname: str, date: datetime) -> pd.DataFrame:
+    return registres[registres[colname] > date]
