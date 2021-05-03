7504b03d8cfd6795c001e22fe9d03ee8b153c515
escardill <nil.celisfont@actuatech.ad>
2021-05-03 10:56:54 +0200

    Ingestor Nan values handling improved

--- Ingestion/date_parser.py
+++ Ingestion/date_parser.py
@@ -3,24 +3,23 @@ import numpy as np
 
 
 def date_parser(date: int) -> datetime:
-    """ Parse DATA_ALTA and DATA-BAIXA columns
+    """ Parse DATA_ALTA and DATA-BAIXA columns of "Registre de vehicles Excel file"
 
     :param date: Date in format type %Y%m%d, ex: 19510915
     :return: datetime of the date or nan
     """
 
-    if date is not np.nan:
-        date = str(date)
-        try:
-            result = datetime.strptime(date, '%Y%m%d')
-            return result
-        except ValueError as e:
-            try:
-                date = date[:4]
-                result = datetime.strptime(date, '%Y')
-                return result
-            except ValueError:
-                print(f'Unable to parse date {date}, with function date_parser, error: {e}')
-                return date
-    else:
-        return date
+    date = str(date)
+    try:
+        result = datetime.strptime(date, '%Y%m%d')
+        return result
+    except ValueError as e:
+        if date != 'nan':
+            if date != '':
+                try:
+                    date = date[:4]
+                    result = datetime.strptime(date, '%Y')
+                    return result
+                except ValueError:
+                    print(f'Unable to parse date {date}, with function date_parser, error: {e}')
+                    return date
