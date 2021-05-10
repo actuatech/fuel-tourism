8c91ec91cf89c203a731455d06b04888877e61f9
escardill <nil.celisfont@actuatech.ad>
2021-05-10 17:35:06 +0200

    drop vehicles agricols and sense carburant functions added

--- DataCleaning/Filters.py
+++ DataCleaning/Filters.py
@@ -2,5 +2,17 @@ from datetime import datetime
 import pandas as pd
 
 
-def filter_by_year_greater_than(registres: pd.DataFrame, colname: str, date: datetime) -> pd.DataFrame:
-    return registres[registres[colname] > date]
+def filter_by_year_greater_than(registre_df: pd.DataFrame, colname: str, date) -> pd.DataFrame:
+    return registre_df[registre_df[colname] > date]
+
+
+def drop_vehicles_agricols(registre_df: pd.DataFrame) -> pd.DataFrame:
+    num_vehicles_agricols = registre_df[registre_df.CARBURANT == 'SENSE CARBURANT'].shape[0]
+    print(f'Total number of vehicles agricols deleted: {num_vehicles_agricols}')
+    return registre_df[registre_df['TIPUS'] != 'VEHICLES AGRICOLS']
+
+
+def drop_vehicles_sense_carburant(registre_df: pd.DataFrame) -> pd.DataFrame:
+    num_vehicles_sense_carburant = registre_df[registre_df.CARBURANT == 'SENSE CARBURANT'].shape[0]
+    print(f'Total number of vehicles SENSE CARBURANT deleted: {num_vehicles_sense_carburant}')
+    return registre_df[registre_df['CARBURANT'] != 'SENSE CARBURANT']
---
+++ DataCleaning/__init__.py
@@ -0,0 +1,6 @@
+from .Filters import (
+    drop_vehicles_agricols,
+    filter_by_year_greater_than,
+    drop_vehicles_sense_carburant
+)
+
