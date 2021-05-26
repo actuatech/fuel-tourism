66401914791a9e830b8948407e7927506c5c82b8
escardill <nil.celisfont@actuatech.ad>
2021-05-26 15:05:03 +0200

    Main file that takes in the vehicles registration data and ouputs a csv with corresponding stock and activity

--- main.py
+++ main.py
@@ -1,15 +1,103 @@
 import pandas as pd
 from pathlib import Path
-from Ingestion import register_ingestor_function
+from datetime import datetime
 
+from Ingestion import register_ingestor_function
+from DataCleaning import (
+    filter_by_year_greater_than,
+    keep_row_if_na_in_column,
+    drop_vehicles_with_no_fuel_associated,
+    drop_agricultural_vehicles,
+    filter_by_year_smaller_than
+                          )
+from Classification import (
+    category_fuel_segment_euro_classification_wrapper_function,
+    MAPPING_CATEGORY_LAST_EURO_STANDARD
+)
+from Activity import (
+    activity_time_and_km_between_itv_revisions,
+    check_for_activity_outliers,
+    calculate_activity_outliers_thresholds,
+    mean_activity_calculator_by_grouping
+)
+
+# ----------
+# PARAMETERS
+# ----------
+# ITV original data filename (Parameter)
+filename_registre_vehicles = '01FEB2021_Historic_vehicles_amb_ITVs.xlsx'
+
+# Years between from which data is keeped
+MIN_YEAR = 1989
+MAX_DATE = datetime(2021, 1, 1)
+
+MIN_DAYS_BETWEEN_REVISIONS = 300
+MIN_STOCK_FOR_MEAN_ACTIVITY_CALCULATION = 50  # Min numb of vehicles in a given grouping to take the mean activity valid
+
+# Output filename for stock and activity dataframe
+filename_output_stock_activity = f'stock_activity_{datetime.now().date()}.csv'
+
+# ----
+# CODE
+# ----
 # Define the current working directory
 cwd = Path.cwd()
-
-# Registre vehicles data
-filename_registre_vehicles = '01FEB2021_Historic_vehicles_amb_ITVs.xlsx'
 path_registre_vehicles = cwd / '_data' / filename_registre_vehicles
 
+# LOADING DATA
 print('Loading registre de vehicles')
-registres = register_ingestor_function(path_registre_vehicles)
+itv_raw = register_ingestor_function(path_registre_vehicles)
+print(f'Total number of vehicles loaded: {itv_raw.shape[0]}')
+print('-')
 
+# DATA CLEANING
+print('Cleaning Data')
+# Keep only last 30 years of data
+vehicles_last_30_years = filter_by_year_greater_than(itv_raw, 'ANY_FABRICACIO', MIN_YEAR)
+vehicles_last_30_years = filter_by_year_smaller_than(vehicles_last_30_years, 'DATA_ALTA', MAX_DATE)
+# Keep only vehicles that are not decommissioned
+vehicles_last_30_years_active_today = keep_row_if_na_in_column(vehicles_last_30_years, 'DATA_BAIXA')
+# Drop agricultural and vehicles with missing Fuel data
+good_vehicles_df = drop_vehicles_with_no_fuel_associated(drop_agricultural_vehicles(vehicles_last_30_years_active_today))
+print(f'Total number of vehicles taken into account: {good_vehicles_df.shape[0]}')
+print('-')
+
+# CATEGORIZING VEHICLES
+print('Starting Vehicle Classification')
+categorized_vehicles_df = category_fuel_segment_euro_classification_wrapper_function(good_vehicles_df)
+
+# STOCK CONFIGURATION AND ACTIVITY CALCULATION
+# Create columns Mileage, number of days and corresponding Activity for each vehicle
+categorized_vehicles_df['Num_of_days'], categorized_vehicles_df['Mileage'], categorized_vehicles_df['Activity'] = zip(
+    *categorized_vehicles_df.apply(lambda row: activity_time_and_km_between_itv_revisions(
+        row, MAX_DATE, MIN_DAYS_BETWEEN_REVISIONS), axis=1))
+
+# Assign to nan Activity outliers
+activity_outliers_per_category_mapping = calculate_activity_outliers_thresholds(categorized_vehicles_df)
+categorized_vehicles_df['Activity'] = categorized_vehicles_df.apply(
+    lambda row: check_for_activity_outliers(row, activity_outliers_per_category_mapping), axis=1)
+# Create Stock Column
+categorized_vehicles_df['COUNT'] = 1
+
+# Calculate stock and mean activity for each partition
+stock_and_mileage_df = categorized_vehicles_df.groupby(
+            ['Category', 'Fuel', 'Segment', 'Euro Standard'], dropna=False, as_index=False).agg(
+            {'Activity': 'mean', 'COUNT': 'sum', 'Mileage': 'sum'}).rename({'Activity': 'Mean_Activity'}, axis=1)
+
+# Calculate mean activity for partitions with no mean activity by assigning them from different partitions
+stock_and_mileage_df['Mean_Activity'] = stock_and_mileage_df.apply(
+    lambda row: mean_activity_calculator_by_grouping(row, categorized_vehicles_df, MAPPING_CATEGORY_LAST_EURO_STANDARD,
+                                                     MIN_STOCK_FOR_MEAN_ACTIVITY_CALCULATION), axis=1)
+try:
+    # TODO: veure perquè passar això i solucionar-ho
+    stock_and_mileage_df['Mean_Activity'] = stock_and_mileage_df['Mean_Activity'].astype(int)
+    stock_and_mileage_df['Mileage'] = stock_and_mileage_df['Mileage'].astype(int)
+except Exception:
+    print('Check for nan values in the stock_and_mileage dataframe: Mean activity or Mileage')
+
+# Save results
+try:
+    stock_and_mileage_df.to_csv(filename_output_stock_activity)
+except Exception:
+    raise Exception('Unable to save stock and activity to file')
 print('end')
